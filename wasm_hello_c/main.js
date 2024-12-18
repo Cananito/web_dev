266d99783c1940e9b3d68cd29e5171151e8eb55d
Rogelio Gudino <cananito@gmail.com>
2024-12-17 22:55:36 -0800

    Adds function import example to wasm_hello_c.

--- wasm_hello_c/main.js
+++ wasm_hello_c/main.js
@@ -1,17 +1,52 @@
 let resultLabel;
+let helloLabel;
+let wasmObj;
+
+function string_from_pointer(pointer, length) {
+  const wasmBuffer = wasmObj.instance.exports.memory.buffer;
+  const string_bytes = new Uint8Array(wasmBuffer, pointer, length);
+  let string = new TextDecoder().decode(string_bytes);
+  return string;
+}
+
+function r_js_print(message, message_length) {
+  helloLabel.innerHTML = string_from_pointer(message, message_length);
+}
 
 function initializeUI() {
+  const body = document.body;
+
   resultLabel = document.createElement("span");
   resultLabel.innerHTML = "Loading...";
-  const body = document.body;
   body.appendChild(resultLabel);
+
+  body.appendChild(document.createElement("br"));
+
+  helloLabel = document.createElement("span");
+  helloLabel.innerHTML = "Loading...";
+  body.appendChild(helloLabel);
 }
 
 function main() {
   initializeUI();
-  WebAssembly.instantiateStreaming(fetch("some.wasm"), {}).then(function (obj) {
-    const result = obj.instance.exports.r_sum(1, 6);
+
+  const importObject = {
+    env: {
+      "print": r_js_print,
+    },
+  };
+
+  const wasmPromise = WebAssembly.instantiateStreaming(fetch("some.wasm"),
+                                                       importObject);
+  wasmPromise.then(function (obj) {
+    wasmObj = obj;
+
+    const exports = obj.instance.exports;
+
+    const result = exports.r_sum(1, 6);
     resultLabel.innerHTML = "Result: " + result;
+
+    exports.r_call_me_back();
   });
 };
 document.addEventListener("DOMContentLoaded", main);
--- wasm_hello_c/some.c
+++ wasm_hello_c/some.c
@@ -1,3 +1,21 @@
 int r_sum(int a, int b) {
   return a + b;
 }
+
+extern void print(char* message, int message_length);
+
+static int r_strlen(char* str) {
+  int len = 0;
+  int i = 0;
+  while (str[i] != '\0') {
+    len++;
+    i++;
+  }
+  return len;
+}
+
+void r_call_me_back(void) {
+  char* message = "Hello from C!";
+  int message_length = r_strlen(message);
+  print(message, message_length);
+}
